4046cb055061f88921f709c92ed310b906184615
grkmkctrk <65461375+grkmkctrk@users.noreply.github.com>
2020-12-19 01:56:13 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+from main import stanfordT
+
+
+uret = stanfordT(
+    theta = [0,90, 0, 90,270,0],
+    d = [50, 40], # d3 d6
+    a2 = [40] # a2
+    )
+
+print(uret.matrix())
+print(uret.pointInPlane([0,0,10], axis = 'global'))
